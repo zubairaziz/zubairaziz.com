9fb3c51127da7c236a7768c41fd61c842e541f7f
Zubair Aziz <zabaziz@trainor.com>
2020-05-23 21:36:13 -0400

    Portfolio section

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,31 +1,71 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Image from 'gatsby-image'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import PageHeader from '../components/page-header'
 import Container from '../components/container'
 import Grid from '../components/grid'
+import Card from '../components/card'
 
-class Portfolio extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const imageData = data.backgroundImage.childImageSharp.fluid
+const Portfolio = (props) => {
+  const { data } = props
+  const siteTitle = data.site.siteMetadata.title
+  const imageData = data.backgroundImage.childImageSharp.fluid
+  const sites = data.allMdx.edges
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Portfolio"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        <PageHeader pageTitle={`Portfolio`} imageData={imageData} />
-        <Container>
-          <Grid></Grid>
-        </Container>
-      </Layout>
-    )
-  }
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        title="Portfolio"
+        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+      />
+      <PageHeader pageTitle={`Portfolio`} imageData={imageData} />
+      <Container>
+        <Card>
+          <h2>Websites</h2>
+          <Grid>
+            {sites.map(({ node }, index) => {
+              console.log(node)
+              const title = node.frontmatter.title
+              const summary = node.frontmatter.summary
+              const technologies = node.frontmatter.technologies
+              const featuredImgFluid =
+                node.frontmatter.featuredImage.childImageSharp.fluid
+              return (
+                <a
+                  data-sal="slide-up"
+                  data-sal-delay={`${index}00`}
+                  data-sal-duration="500"
+                  data-sal-easing="ease-in-out-sine"
+                  key={index}
+                  href={node.frontmatter.url}
+                  target="_blank"
+                  className="no-underline portfolio-list-item transition-shadow duration-300 ease-in-out shadow-lg hover:shadow-2xl"
+                >
+                  <Image fluid={featuredImgFluid} className="rounded-t-md" />
+                  <div className="no-underline p-4 bg-gray-800 text-gray-100 rounded-b-md">
+                    <div className="no-underline font-semibold text-lg">
+                      {title}
+                    </div>
+                    <div className="no-underline">{summary}</div>
+                    <ul className="no-underline">
+                      {technologies.map((technology, index) => (
+                        <li className="no-underline" key={index}>
+                          {technology}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </a>
+              )
+            })}
+          </Grid>
+        </Card>
+      </Container>
+    </Layout>
+  )
 }
 
 export default Portfolio
@@ -41,6 +81,29 @@ export const pageQuery = graphql`
       childImageSharp {
         fluid(quality: 90, maxWidth: 1920) {
           ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { fileAbsolutePath: { regex: "/(portfolio)/" } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            url
+            summary
+            technologies
+            featuredImage {
+              childImageSharp {
+                fluid(maxWidth: 800) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
         }
       }
     }
--- wrap-root-element.js
+++ wrap-root-element.js
@@ -6,6 +6,14 @@ import { preToCodeBlock } from 'mdx-utils'
 // components is its own object outside of render so that the references to
 // components are stable
 const components = {
+  h2: ({children}) => (<h2 className="font-semibold mb-4 text-3xl md:text-4xl lg:text-5xl">{children}</h2>),
+  h3: ({children}) => (<h3 className="font-semibold mb-4 text-2xl md:text-3xl lg:text-4xl">{children}</h3>),
+  h1: ({children}) => (<h1 className="font-semibold mb-4 text-4xl md:text-5xl lg:text-6xl">{children}</h1>),
+  h4: ({children}) => (<h4 className="font-semibold mb-4 text-xl md:text-2xl lg:text-3xl">{children}</h4>),
+  h5: ({children}) => (<h5 className="font-semibold mb-4 text-lg md:text-xl lg:text-2xl">{children}</h5>),
+  h6: ({children}) => (<h6 className="font-semibold mb-4 text-base md:text-lg lg:text-xl">{children}</h6>),
+  p: ({children}) => (<p className="text-base lg:text-lg mb-1">{children}</p>),
+
   pre: preProps => {
     const props = preToCodeBlock(preProps)
     // if there's a codeString and some props, we passed the test
