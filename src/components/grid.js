365cb4b0ae0518f50b716bab1ec5d5e9224b2d15
Zubair Aziz <zabaziz@trainor.com>
2020-05-24 23:54:34 -0400

    Fix grid on small sizes

--- src/components/grid.js
+++ src/components/grid.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Grid = (props) => {
   const { children } = props
   return (
-    <div className="grid sm:gap-2 md:gap-4 lg:gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-5 mb-10">
+    <div className="grid gap-2 md:gap-4 lg:gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-5 mb-10">
       {children}
     </div>
   )
